083a5efbbc744ed3f9110e2d05bcb00a58ba8085
ahmedmustahid <ahmed@epx.phys.tohoku.ac.jp>
2023-07-17 16:23:03 +0900

    made demo call to asterisk

--- media/demo.py
+++ media/demo.py
@@ -5,6 +5,9 @@ import os
 import logging
 import pjsua2 as pj
 
+import hydra
+from omegaconf import DictConfig, OmegaConf
+
 # ep = pj.Endpoint()
 ep = None
 
@@ -48,8 +51,10 @@ class Account(pj.Account):
     def __init__(self):
         super().__init__()
 
+@hydra.main(version_base=None, config_path="../conf", config_name="config")
+def main(cfg : DictConfig):
+    print(OmegaConf.to_yaml(cfg))
 
-def main():
     # Create and initialize the library
     global ep
     ep_cfg = pj.EpConfig()
@@ -72,10 +77,17 @@ def main():
     # Start the library
     ep.libStart()
 
+    sipServerIP = cfg.sipServer.ip 
+    sipServerPort = cfg.sipServer.port
+    sipServerUsername = cfg.sipServer.username
+    sipServerPassword = cfg.sipServer.password 
     acfg = pj.AccountConfig()
-    acfg.idUri = "sip:1@10.7.205.149"
-    cred = pj.AuthCredInfo("digest", "*", "1", 0, "test")
+    idUri = "sip:"+sipServerUsername+"@"+sipServerIP+":"+str(sipServerPort)
+    print(f"sending request to {idUri}")
+    acfg.idUri = idUri
+    cred = pj.AuthCredInfo("digest", "*", sipServerUsername, 0, sipServerPassword)
     acfg.sipConfig.authCreds.append(cred)
+
 
     # Create the account
     acc = Account()
@@ -87,7 +99,7 @@ def main():
     call_param.opt.audioCount = 1
     call_param.opt.videoCount = 0
     call_prm = pj.CallOpParam(True)
-    call.makeCall("sip:2@10.7.205.149", call_prm)
+    call.makeCall(idUri , call_prm)
 
     # while not call.done:
     while True:
